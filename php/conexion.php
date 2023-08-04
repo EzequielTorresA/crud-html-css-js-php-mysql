45b02a9f6e88dc0a2ce6bb06a9b2883f0a88d08a
ExE <torres_a_ezequiel@hotmail.com>
2023-08-04 15:42:01 -0700

    Pulido de codigo y comienzo del php + base de datos creada

---
+++ php/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+
+$db = "proyecto-crud";
+$conexion = mysqli_connect($host, $user, $pass, $db)
+
+//Aquí hay una condición IF, checate eso después
